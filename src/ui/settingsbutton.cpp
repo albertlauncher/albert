35c6d75087d01ccafdd286bd59ab27a08656b393
Manuel Schneider <manuelschneid3r@gmail.com>
2014-11-04 02:54:55 +0100

    Use svg as mask for the settings button

--- src/ui/settingsbutton.cpp
+++ src/ui/settingsbutton.cpp
@@ -16,49 +16,37 @@
 
 #include "settingsbutton.h"
 #include "settingsdialog.h"
-#include <QApplication>
 #include <QPaintEvent>
 #include <QPainter>
-#include <QDebug>
-#include <QPolygonF>
-#include <qmath.h>
 
 SettingsButton::SettingsButton(QWidget *parent) :
 	QPushButton(parent)
 {
 	this->setObjectName(QString::fromLocal8Bit("settingsbutton"));
 	this->setFocusPolicy(Qt::NoFocus);
-//	setAttribute(Qt::WA_TranslucentBackground);
 }
-
 
 void SettingsButton::paintEvent(QPaintEvent *event)
 {
-	QPushButton::paintEvent(event);
-	QRectF r(event->rect());
-	r.adjust(1,1,-1,-1);
+	QPushButton::paintEvent(event); // TODO REPLACE BY CORRECT STYLEFUNC
+	//	style()->drawPrimitive(QStyle::PE_FrameDefaultButton, &option, &p, this);
+	//	style()->drawControl(QStyle::CE_PushButton,&option,&painter);
 
-	QPixmap icon = QPixmap(event->rect().size());
-	icon.fill(Qt::transparent);
+	QStyleOptionButton option;
+	option.initFrom(this);
+	QRect r = this->rect();
 
-	// Draw a simple circle TODO QTBUG-42337
-	QPainter p1(&icon);
-	p1.setRenderHint(QPainter::Antialiasing, true);
-	p1.setPen(this->palette().brush(QPalette::Active, QPalette::WindowText).color());
-	p1.setBrush(QBrush(this->palette().brush(QPalette::Active, QPalette::WindowText).color()));
-	p1.drawEllipse(r);
+	// Prepare miage in pixmap using mask
+	QPixmap pm(r.size());
+	pm.fill(Qt::transparent);
+	pm.fill(option.palette.windowText().color());
+	QIcon w(":cogwheel");
+	QPainter ppm(&pm);
+	ppm.setCompositionMode(QPainter::CompositionMode_DestinationIn);
+	ppm.drawPixmap(0,0,w.pixmap(r.size()));
 
-	// Cut the cogs of the wheel
-	p1.setCompositionMode(QPainter::CompositionMode_DestinationOut);
-	double rad=r.width()/7;
-	for (int i = 0; i < 6; ++i)
-	{
-		QPointF c(r.center().x() + qCos(2*M_PI*i/5)*r.width()/2,
-				 r.center().y() +  qSin(2*M_PI*i/5)*r.height()/2);
-		p1.drawEllipse(c,rad,rad);
-	}
-	p1.drawEllipse(r.center(),rad, rad);
-
-	QPainter p2(this);
-	p2.drawPixmap(0,0,icon);
+	// Draw pixmap on button
+	QPainter p(this);
+	p.setRenderHint(QPainter::Antialiasing, true);
+	p.drawPixmap(0,0,pm);
 }
