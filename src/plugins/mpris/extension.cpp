aff04dc11be15121640c785af2397fd67ce7c26a
idkCpp <martin.buergmann@gmail.com>
2016-05-11 16:06:57 +0200

    Added and changed copyright notice

--- src/plugins/mpris/extension.cpp
+++ src/plugins/mpris/extension.cpp
@@ -1,5 +1,5 @@
-// albert - a simple application launcher for linux
-// Copyright (C) 2014-2015 Manuel Schneider
+// albert extension mpris - a mpris interface plugin for albert
+// Copyright (C) 2016 Martin Buergmann
 //
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
