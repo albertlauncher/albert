adde8f7e54270433e6cad59721887d8917e140bc
idkCpp <martin.buergmann@gmail.com>
2017-07-24 21:52:57 +0200

    Adding user option to ssh

---
+++ src/plugins/ssh/src/sshitem.h
@@ -0,0 +1,43 @@
+// albert - a simple application launcher for linux
+// Copyright (C) 2016-2017 Martin Buergmann
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+#pragma once
+#include <QString>
+#include <vector>
+#include <memory>
+#include "core_globals.h"
+#include "standarditem.h"
+
+namespace Ssh {
+
+class Item : public Core::StandardItem
+{
+public:
+
+    Item(const QString &id = QString(), const QString &connector = QString());
+    Item(const Item& copy);
+
+    void activate() const;
+
+    void setConnector(const QString& connector);
+
+protected:
+
+    QString connector_;
+
+};
+
+}
