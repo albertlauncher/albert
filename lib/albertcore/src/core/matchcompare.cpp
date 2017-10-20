a7e2e9f48a67acddbe8f1eae9b496b3ba0a25676
Manuel Schneider <manuelschneid3r@gmail.com>
2017-10-20 17:17:38 +0200

    Fix new sort order (shorter srtings first)

--- lib/albertcore/src/core/matchcompare.cpp
+++ lib/albertcore/src/core/matchcompare.cpp
@@ -23,9 +23,9 @@ bool Core::MatchCompare::operator()(const pair<shared_ptr<Item>, uint> &lhs,
                                   const pair<shared_ptr<Item>, uint> &rhs) {
     // Compare urgency then score
     if (lhs.first->urgency() != rhs.first->urgency())
-        return lhs.first->urgency() > rhs.first->urgency();
+        return lhs.first->urgency() < rhs.first->urgency();
     else if (lhs.second != rhs.second)
         return lhs.second > rhs.second; // Compare match score
     else
-        return lhs.first->text().size() > rhs.first->text().size();
+        return lhs.first->text().size() < rhs.first->text().size();
 }
