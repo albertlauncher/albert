a66683241194713e1c40557d6f7da2849f39fadb
Manuel Schneider <manuelschneid3r@gmail.com>
2017-11-25 18:24:30 +0100

    v0.14.13

--- lib/albertcore/src/core/albert.cpp
+++ lib/albertcore/src/core/albert.cpp
@@ -83,7 +83,7 @@ int Core::AlbertApp::run(int argc, char **argv) {
         app = new QApplication(argc, argv);
         app->setApplicationName("albert");
         app->setApplicationDisplayName("Albert");
-        app->setApplicationVersion("v0.14.12");
+        app->setApplicationVersion("v0.14.13");
 
         // Parse commandline
         qDebug() << "Parsing commandline";
