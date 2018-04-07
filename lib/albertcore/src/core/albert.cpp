5743086e43dfbbca5f817abc02eeae303974a5b7
Manuel Schneider <manuelschneid3r@gmail.com>
2018-04-07 17:19:52 +0200

    v0.14.19

--- lib/albertcore/src/core/albert.cpp
+++ lib/albertcore/src/core/albert.cpp
@@ -111,7 +111,7 @@ int Core::AlbertApp::run(int argc, char **argv) {
         app = new QApplication(argc, argv);
         app->setApplicationName("albert");
         app->setApplicationDisplayName("Albert");
-        app->setApplicationVersion("v0.14.18");
+        app->setApplicationVersion("v0.14.19");
 
         if (parser.isSet("debug"))
             printDebugOutput = true;
