d235776d142e9f7c3a8f702f43722f323b54e48c
Manuel Schneider <manuelschneid3r@gmail.com>
2018-09-21 18:27:04 +0200

    Fix travis #2

--- lib/albertcore/src/core/queryexecution.h
+++ lib/albertcore/src/core/queryexecution.h
@@ -1,29 +1,16 @@
-// albert - a simple application launcher for linux
-// Copyright (C) 2014-2017 Manuel Schneider
-//
-// This program is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-//
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License
-// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+// Copyright (C) 2014-2018 Manuel Schneider
 
 #pragma once
 #include <QAbstractListModel>
 #include <QFuture>
 #include <QFutureWatcher>
 #include <QTimer>
-#include <set>
+#include <chrono>
 #include <map>
-#include <vector>
-#include <utility>
 #include <memory>
+#include <set>
+#include <utility>
+#include <vector>
 #include "query.h"
 
 namespace Core {
@@ -60,7 +47,7 @@ public:
                    const QString &queryString,
                    std::map<QString,uint> scores,
                    bool fetchIncrementally);
-    ~QueryExecution();
+    ~QueryExecution() override;
 
     const State &state() const;
 
