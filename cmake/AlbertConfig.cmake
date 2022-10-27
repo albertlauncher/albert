72690ae4e253f56c6904e84e24ad9c394a442ced
Manuel Schneider <manuelschneid3r@gmail.com>
2022-10-27 22:38:55 +0200

    CMake make plugin ID optional use dir name

--- cmake/AlbertConfig.cmake
+++ cmake/AlbertConfig.cmake
@@ -10,36 +10,9 @@
 ####################### nifty stuff for library developers
 
 # outside because used by albert_plugin_generate_metadata_json and albert_plugin
-set(md_bool NOUSER FRONTEND)
-set(md_vals ID VERSION NAME DESCRIPTION LICENSE URL)
-set(md_list MAINTAINERS QT_DEPENDENCIES LIB_DEPENDENCIES EXEC_DEPENDENCIES)
+
 
 function(albert_plugin_generate_metadata_json)
-    cmake_parse_arguments(MD "${md_bool}" "${md_vals}" "${md_list}" ${ARGN})
-
-    if (NOT DEFINED MD_ID)
-        message(FATAL_ERROR "${PROJECT_NAME}: Plugin identifier is undefined")
-    endif()
-    if (NOT DEFINED MD_VERSION)
-        message(FATAL_ERROR "Plugin version is undefined")
-    endif()
-    if (NOT DEFINED MD_NAME)
-        message(FATAL_ERROR "Plugin name is undefined")
-    endif()
-    if (NOT DEFINED MD_DESCRIPTION)
-        message(FATAL_ERROR "Plugin description is undefined")
-    endif()
-    if (NOT DEFINED MD_LICENSE)
-        message(FATAL_ERROR "Plugin license is undefined")
-    endif()
-    if (NOT DEFINED MD_URL)
-        message(FATAL_ERROR "Plugin url is undefined")
-    endif()
-    if (DEFINED NOUSER AND DEFINED FRONTEND)
-        message(FATAL_ERROR "Conflict: NOUSER and FRONTED specified.")
-    endif()
-
-
     set(MD "{}")
     string(JSON MD SET ${MD} "id" "\"${MD_ID}\"")
     string(JSON MD SET ${MD} "version" "\"${MD_VERSION}\"")
@@ -48,9 +21,9 @@ function(albert_plugin_generate_metadata_json)
     string(JSON MD SET ${MD} "license" "\"${MD_LICENSE}\"")
     string(JSON MD SET ${MD} "url" "\"${MD_URL}\"")
 
-    if (DEFINED NOUSER)
+    if (MD_NOUSER)
         string(JSON MD SET ${MD} "type" "\"none\"")
-    elseif(DEFINED FRONTEND)
+    elseif(MD_FRONTEND)
         string(JSON MD SET ${MD} "type" "\"frontend\"")
     else()
         string(JSON MD SET ${MD} "type" "\"user\"")
@@ -85,20 +58,15 @@ function(albert_plugin_generate_metadata_json)
 
     # Create the metadata in the build dir
     file(WRITE "${CMAKE_CURRENT_BINARY_DIR}/metadata.json" "${MD}")
-#    message("${CMAKE_CURRENT_BINARY_DIR}/metadata.json ${MD}")
+    #    message("${CMAKE_CURRENT_BINARY_DIR}/metadata.json ${MD}")
 endfunction()
 
 function(albert_plugin_add_default_target)
-    # Add a target
     file(GLOB_RECURSE SRC *.cpp src/*.cpp *.qrc *.ui)
     #message("${PROJECT_NAME} ${SRC}")
     add_library(${PROJECT_NAME} SHARED ${SRC})
     add_library(albert::${PROJECT_NAME} ALIAS ${PROJECT_NAME})
     target_link_libraries(${PROJECT_NAME} PRIVATE albert::albert)
-
-    # Add the metadata as a macro
-    #    target_compile_definitions(${PROJECT_NAME} PRIVATE PROJECT_NAME="${PROJECT_NAME}")
-    #    target_compile_definitions(${PROJECT_NAME} PRIVATE PROJECT_METADATA='${METADATA}')
 endfunction()
 
 function(albert_plugin_generate_export_header)
@@ -110,13 +78,13 @@ function(albert_plugin_add_default_target_properties)
             CXX_VISIBILITY_PRESET hidden
             VISIBILITY_INLINES_HIDDEN 1
             INSTALL_RPATH "$ORIGIN"
-    )
+            )
 endfunction()
 
 function(albert_plugin_add_default_install)
     install(TARGETS ${PROJECT_NAME}
             LIBRARY DESTINATION ${CMAKE_INSTALL_LIBDIR}/albert/plugins
-    )
+            )
 endfunction()
 
 function(albert_plugin_add_qt_dependencies)
@@ -126,23 +94,46 @@ function(albert_plugin_add_qt_dependencies)
     endforeach()
 endfunction()
 
-function(albert_plugin_default)
-    albert_plugin_add_default_target()
-    albert_plugin_add_default_target_properties()
-    albert_plugin_add_default_install()
-    albert_plugin_add_qt_dependencies(${ARGV})
-endfunction()
-
 function(albert_plugin)
+    set(md_bool NOUSER FRONTEND)
+    set(md_vals ID VERSION NAME DESCRIPTION LICENSE URL)
+    set(md_list MAINTAINERS QT_DEPENDENCIES LIB_DEPENDENCIES EXEC_DEPENDENCIES)
     cmake_parse_arguments(MD "${md_bool}" "${md_vals}" "${md_list}" ${ARGV})
+
+    if (NOT DEFINED MD_ID)
+        get_filename_component(MD_ID ${CMAKE_CURRENT_SOURCE_DIR} NAME)
+    endif()
+
+    if (NOT DEFINED MD_VERSION)
+        message(FATAL_ERROR "Plugin version is undefined")
+    endif()
+
+    if (NOT DEFINED MD_NAME)
+        message(FATAL_ERROR "Plugin name is undefined")
+    endif()
+
+    if (NOT DEFINED MD_DESCRIPTION)
+        message(FATAL_ERROR "Plugin description is undefined")
+    endif()
+
+    if (NOT DEFINED MD_LICENSE)
+        message(FATAL_ERROR "Plugin license is undefined")
+    endif()
+
+    if (NOT DEFINED MD_URL)
+        message(FATAL_ERROR "Plugin url is undefined")
+    endif()
+
+    if (MD_NOUSER AND MD_FRONTEND)
+        message(FATAL_ERROR "Conflict: NOUSER and FRONTEND specified.")
+    endif()
 
     project(${MD_ID} VERSION ${MD_VERSION})
 
-    albert_plugin_generate_metadata_json(${ARGV})
     albert_plugin_add_default_target()
     albert_plugin_add_default_target_properties()
     albert_plugin_generate_export_header()
+    albert_plugin_generate_metadata_json()
     albert_plugin_add_qt_dependencies(${MD_QT_DEPENDENCIES})
     albert_plugin_add_default_install()
-
 endfunction()
