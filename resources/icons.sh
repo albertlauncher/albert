836caca119719e3aa573b4d72b3c963ea2768024
Manuel Schneider <manuelschneid3r@gmail.com>
2022-12-26 14:54:42 +0100

    deployment trial an error

---
+++ resources/icons.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+input_path=app_icon.svg
+output_path=/tmp/output.iconset
+mkdir "$output_path"
+
+# the convert command comes from imagemagick
+for size in 16 32 64 128 256; do
+  half="$(($size / 2))"
+  convert ${input_path} -resize x$size $output_path/icon_${size}x${size}.png
+  convert ${input_path} -resize x$size $output_path/icon_${half}x${half}@2x.png
+done
+
+iconutil -c icns $output_path -o AppIcon.icns
