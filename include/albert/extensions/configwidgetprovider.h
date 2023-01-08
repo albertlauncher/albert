5322c5698d20e5907219cc99cc0ac449cfad4ea0
Manuel Schneider <manuelschneid3r@gmail.com>
2023-01-08 21:19:42 +0100

    Remove obsolte note in interface

--- include/albert/extensions/configwidgetprovider.h
+++ include/albert/extensions/configwidgetprovider.h
@@ -12,7 +12,6 @@ class ALBERT_EXPORT ConfigWidgetProvider : virtual public Extension
 {
 public:
     /// Returns the cofig widget for this plugin
-    /// @note The widgets objectName() is used as list item title
     /// @note Implementations must give away ownership (do not delete!)
     virtual QWidget* buildConfigWidget() = 0;
 };
