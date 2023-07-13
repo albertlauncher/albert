3208074e2a731f95a278223e9fde26721d3af1fc
Manuel Schneider <manuelschneid3r@gmail.com>
2023-07-13 22:41:29 +0200

    Add albert data/conf/cache items

--- src/appqueryhandler.cpp
+++ src/appqueryhandler.cpp
@@ -15,24 +15,60 @@ QString AppQueryHandler::description() const { return "Control the app"; }
 void AppQueryHandler::updateIndexItems()
 {
     auto settings_item = StandardItem::make(
-            "albert-settings", "Settings", "Open the Albert settings window", {":app_icon"},
-            {{"albert-settings", "Open settings", [](){ showSettings(); }}}
+        "albert-settings",
+        "Settings",
+        "Open the Albert settings window",
+        {":app_icon"},
+        {{"albert-settings", "Open settings", [](){ showSettings(); }}}
     );
 
     auto quit_item = StandardItem::make(
-            "albert-quit", "Quit Albert", "Quit this application", {":app_icon"},
-            {{"albert-quit", "Quit Albert", [](){ quit(); }}}
+        "albert-quit",
+        "Quit Albert",
+        "Quit this application",
+        {":app_icon"},
+        {{"albert-quit", "Quit Albert", [](){ quit(); }}}
     );
 
     auto restart_item = StandardItem::make(
-            "albert-restart", "Restart Albert", "Restart this application", {":app_icon"},
-            {{"albert-restart", "Restart Albert", [](){ restart(); }}}
+        "albert-restart",
+        "Restart Albert",
+        "Restart this application",
+        {":app_icon"},
+        {{"albert-restart", "Restart Albert", [](){ restart(); }}}
     );
+
+    auto config = StandardItem::make(
+        "albert-config",
+        "Albert config location",
+        albert::configLocation(),
+        {":app_icon"},
+        {{"open", "Open", [](){ albert::openUrl(albert::configLocation()); }}}
+        );
+
+    auto data = StandardItem::make(
+        "albert-data",
+        "Albert data location",
+        albert::dataLocation(),
+        {":app_icon"},
+        {{"open", "Open", [](){ albert::openUrl(albert::dataLocation()); }}}
+        );
+
+    auto cache = StandardItem::make(
+        "albert-cache",
+        "Albert cache location",
+        albert::cacheLocation(),
+        {":app_icon"},
+        {{"open", "Open", [](){ albert::openUrl(albert::cacheLocation()); }}}
+        );
 
     vector<IndexItem> index_items;
     index_items.emplace_back(settings_item, "settings");
     index_items.emplace_back(settings_item, "preferences");
     index_items.emplace_back(quit_item, "quit");
     index_items.emplace_back(restart_item, "restart");
+    index_items.emplace_back(config, "config");
+    index_items.emplace_back(data, "data");
+    index_items.emplace_back(cache, "cachenf");
     setIndexItems(::move(index_items));
 }
