3424b91e6bfdb8480f1375d104cd7e43b1295cd8
Manuel Schneider <manuelschneid3r@gmail.com>
2023-07-14 13:00:05 +0200

    0.22 require frontends to provide the native window id
    Also some more frontend documentation

--- include/albert/extension/frontend/frontend.h
+++ include/albert/extension/frontend/frontend.h
@@ -12,22 +12,34 @@ namespace albert
 {
 class Query;
 
-/// The interface for albert frontends
+/// The interface for albert frontends.
 class ALBERT_EXPORT Frontend : virtual public albert::Extension
 {
 public:
+    /// Visibility of the frontend
     virtual bool isVisible() const = 0;
+
+    /// Set the visibility state of the frontend
     virtual void setVisible(bool visible) = 0;
 
+    /// Input line text
     virtual QString input() const = 0;
+
+    /// Input line text setter
     virtual void setInput(const QString&) = 0;
 
+    /// The native window id. Used to apply platform quirks.
+    virtual unsigned long long winId() const = 0;
+
+    /// The config widget show in the window settings tab
     virtual QWidget *createFrontendConfigWidget() = 0;
 
+    /// The query object factory
+    /// @note the QueryEngine is not available in the constructor. Do not use before albert:PluginInstance::initialize
     std::shared_ptr<Query> query(const QString &query) const;
 
 private:
-    void setEngine(QueryEngine*);  // sure private?
+    void setEngine(QueryEngine*);
     QueryEngine *query_engine;
     friend class ::App;
 };
