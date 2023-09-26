f94ce65ddb0613fa7d8b181bd3721328f984fcbb
Manuel Schneider <manuelschneid3r@gmail.com>
2023-09-26 15:12:09 +0200

    Restore 6.2 backward compatibility

--- src/qtpluginprovider.cpp
+++ src/qtpluginprovider.cpp
@@ -51,7 +51,7 @@ QtPluginProvider::QtPluginProvider(QStringList additional_paths):
         QDirIterator dirIterator(path, QDir::Files);
         while (dirIterator.hasNext()) {
             try {
-                auto loader = make_unique<QtPluginLoader>(*this, dirIterator.nextFileInfo().absoluteFilePath());
+                auto loader = make_unique<QtPluginLoader>(*this, QFileInfo(dirIterator.next()).absoluteFilePath());
                 DEBG << "Found valid native plugin" << loader->path;
                 plugins_.push_back(::move(loader));
             } catch (const runtime_error &e) {
