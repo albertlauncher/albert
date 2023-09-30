2881f13f0a3b2dbaac4181cb8dfdbc8e954f04af
Manuel Schneider <manuelschneid3r@gmail.com>
2023-09-30 21:31:32 +0200

    Visually inform users about past crashes

--- src/scopedcrashindicator.cpp
+++ src/scopedcrashindicator.cpp
@@ -1,16 +1,18 @@
 // Copyright (c) 2023 Manuel Schneider
 
-#include "albert/logging.h"
 #include "albert/albert.h"
+#include "albert/logging.h"
 #include "scopedcrashindicator.h"
 #include <QFile>
+#include <QMessageBox>
 #include <QString>
+#include <QGuiApplication>
 
 ScopedCrashIndicator::ScopedCrashIndicator()
 {
     QString filePath = albert::cacheLocation() + "/running";
     if (QFile::exists(filePath)){
-        CRIT << "Application has not been terminated graciously";
+        GWARN("Albert has not been terminated properly. Please check your crash reports and report an issue.");
     } else {
         DEBG << "Creating crash indicator file";
         QFile file(filePath);
@@ -23,5 +25,6 @@ ScopedCrashIndicator::ScopedCrashIndicator()
 ScopedCrashIndicator::~ScopedCrashIndicator()
 {
     INFO << "Removing crash indicator file";
-    QFile::remove(albert::cacheLocation() + "/running");
+    if (!QFile::remove(albert::cacheLocation() + "/running"))
+        WARN << "Removing crash indicator file failed.";
 }
