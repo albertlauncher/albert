88d0cd1a5332a70aa960a31263b5d453f8260ecb
Manuel Schneider <manuelschneid3r@gmail.com>
2023-11-03 13:54:16 +0100

    Document QStyle::StandardPixmap icon provider

--- include/albert/util/iconprovider.h
+++ include/albert/util/iconprovider.h
@@ -28,10 +28,11 @@ public:
     /// \param size Will get assigned the actual size of the pixmap created
     /// \param requestedSize The size the pixmap should have if possible.
     /// \param url The URL of the pixmap to be created. Supported url schemes:
-    /// - **file**: Url to a local image file.
-    /// - **qfip**: Uses [QFileIconProvider] to get the icon for the file path.
-    /// - **xdg**: performs [freedesktop icon theme specification] lookup (Linux only).
     /// - **qrc** or empty: A QResource file path.
+    /// - **qfip**: Uses [QFileIconProvider] to get the icon for the file path.
+    /// - **qsp**: Icons from [QStyle::StandardPixmap enum].
+    /// - **file**: Url to a local image file.
+    /// - **xdg**: performs [freedesktop icon theme specification] lookup (Linux only).
     /// - **gen**: This is a generic icon factory. All parameters are optioal. It draws a
     ///   background circle and renders text on it. Available parameters are:
     ///   - background: The background color (default: none). See also [QColor::fromString].
@@ -41,18 +42,20 @@ public:
     ///
     ///   Examples
     ///
-    ///       file:/path/to/local/image/file
-    ///       qfip:/some/dir
-    ///       qfip:/but/also/fancy/file/types.pdf
-    ///       xdg:some-themed-icon-name
-    ///       xdg:nautilus
     ///       qrc:nested/path/to/some.svg
     ///       qrc:or-aliased
-    ///       :or-this
+    ///       :also-working-with-empty-sheme
+    ///       qfip:/some/dir
+    ///       qfip:/but/also/fancy/file/types.pdf
+    ///       qsp:SP_TrashIcon
+    ///       file:/path/to/local/image/file
+    ///       xdg:some-themed-icon-name
+    ///       xdg:nautilus
     ///       gen:?background=blue&foreground=red&text=Hi&fontscalar=0.5
     ///
     /// \returns The pixmap, if available, null pixmap otherwise.
     ///
+    /// [QStyle::StandardPixmap enum]: https://doc.qt.io/qt-6/qstyle.html#StandardPixmap-enum
     /// [freedesktop icon theme specification]: https://specifications.freedesktop.org/icon-theme-spec/latest/
     /// [QFileIconProvider]: https://doc.qt.io/qt/qfileiconprovider.html
     /// [QColor::fromString]: https://doc.qt.io/qt/qcolor.html#fromString
