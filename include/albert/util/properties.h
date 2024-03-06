bd0bf3926db65cd9e3cd3d6c210968405cbcbb25
Manuel Schneider <manuelschneid3r@gmail.com>
2024-03-06 18:45:54 +0100

    [API:add] Separate and improve ALBERT_PLUGIN_PROPERTY macros
    * ALBERT_PROPERTY_GETSET
    * ALBERT_PLUGIN_PROPERTY_GETSET
    * ALBERT_PROPERTY_CONNECT_SPINBOX

--- include/albert/util/properties.h
+++ include/albert/util/properties.h
@@ -9,20 +9,22 @@
 /// @brief Convenience macro for (incomplete) user property definition.
 ///
 /// The property this macro produces is _incomplete_. It still requires you
-/// to define
+/// to provide definitions for:
 ///
 /// - the public getter '<type> <name>() const'
 /// - the private setter 'void set_<name>_(<type>)'
 ///
 /// Expands to (psuedo code):
 ///
-/// - static const <type> <name>_default
-/// - void reset_<name>()
-/// - void store_<name>()
-/// - void restore_<name>()
-/// - void set_<name>(<type>)
+/// - QPROPERTY(…)
+/// - <type> <name>_default() {…}
+/// - void reset_<name>() {…}
+/// - void store_<name>() {…}
+/// - void restore_<name>() {…}
+/// - void set_<name>(<type>) {…}
 /// - signal <name>_changed();
-/// - QPROPERTY
+/// - <type> <name>() const;     // Declaration only
+/// - void set_<name>_(<type>);  // Declaration only
 ///
 /// @param type The type of the property
 /// @param name The name of the property
@@ -31,16 +33,17 @@
 ///        object (*, ->). A pointer, factory, etc.
 ///
 #define ALBERT_PROPERTY_BASE(type, name, defaultValue, settings) \
-    public: static const type name##_default{defaultValue}; \
+    public: static type name##_default(){ return defaultValue; }; \
     protected: void store_##name() { settings()->setValue(EXPAND_STRINGIZE(name), name()); } \
     protected: void restore_##name(const auto &s = nullptr) { \
-        if (s) set_##name##_(s->value(EXPAND_STRINGIZE(name), name##_default).template value<type>()); \
-        else set_##name##_(settings()->value(EXPAND_STRINGIZE(name), name##_default).template value<type>()); \
+        if (s) set_##name##_(s->value(EXPAND_STRINGIZE(name), name##_default()).template value<type>()); \
+        else set_##name##_(settings()->value(EXPAND_STRINGIZE(name), name##_default()).template value<type>()); \
     } \
-    public: void reset_##name() { set_##name##_(name##_default); settings()->remove(EXPAND_STRINGIZE(name));  } \
+    public: void reset_##name() { set_##name##_(name##_default()); settings()->remove(EXPAND_STRINGIZE(name));  } \
     Q_SIGNAL void name##_changed(); \
     Q_PROPERTY(type name READ name WRITE set_##name RESET reset_##name NOTIFY name##_changed USER true) \
     public: void set_##name(type val) { if (val != name()){ set_##name##_(val); store_##name(); emit name##_changed(); } }
+
 
 ///
 /// @brief Convenience macro for (incomplete) plugin user property definition.
@@ -52,12 +55,45 @@
 
 
 ///
+/// @brief Convenience macro for (incomplete) user property definition.
+///
+/// Extends ALBERT_PLUGIN_PROPERTY_BASE by (psuedo code):
+///
+/// - <type> <name>() const;     // Declaration only
+/// - void set_<name>_(<type>);  // Declaration only
+///
+/// The property this macro produces is _incomplete_. It still requires you
+/// to provide _definitions_.
+///
+/// @param type The type of the property
+/// @param name The name of the property
+/// @param defaultValue The default value of the property
+/// @param settings Something that evaluates to a  dereferencable QSettings
+///        object (*, ->). A pointer, factory, etc.
+///
+#define ALBERT_PROPERTY_GETSET(type, name, defaultValue, settings) \
+    ALBERT_PROPERTY_BASE(type, name, defaultValue, settings) \
+    public: type name() const; \
+    private: void set_##name##_(type);
+
+
+///
+/// @brief Convenience macro for (incomplete) plugin user property definition.
+///
+/// Calls ALBERT_PROPERTY_BASE with PluginInstance::settings.
+///
+#define ALBERT_PLUGIN_PROPERTY_GETSET(type, name, defaultValue) \
+    ALBERT_PROPERTY_GETSET(type, name, defaultValue, PluginInstance::settings)
+
+
+
+///
 /// @brief Convenience macro for user property definition using a given member.
 ///
 /// Extends ALBERT_PLUGIN_PROPERTY_BASE by (psuedo code):
 ///
-/// - <type> <name>() const
-/// - void set_<name>_(<type>)
+/// - <type> <name>() const {…}
+/// - void set_<name>_(<type>) {…}
 ///
 /// @param type The type of the property
 /// @param name The name of the property
@@ -75,7 +111,7 @@
 /// Calls ALBERT_PROPERTY_MEMBER with PluginInstance::settings.
 ///
 #define ALBERT_PLUGIN_PROPERTY_MEMBER(type, name, member, defaultValue) \
-    ALBERT_PROPERTY_MEMBER(type, name, defaultValue, PluginInstance::settings)
+    ALBERT_PROPERTY_MEMBER(type, name, member, defaultValue, PluginInstance::settings)
 
 
 ///
@@ -91,7 +127,7 @@
 ///
 #define ALBERT_PROPERTY(type, name, defaultValue, settings) \
     ALBERT_PROPERTY_MEMBER(type, name, name##_, defaultValue, settings) \
-    private: type name##_{name##_default};
+    private: type name##_{name##_default()};
 
 ///
 /// @brief Convenience macro for plugin user property definition defining a member.
@@ -128,4 +164,17 @@
 /// @param widget The widget pointer to connect to
 ///
 #define ALBERT_PROPERTY_CONNECT_CHECKBOX(object, name, widget) \
-    ALBERT_PROPERTY_CONNECT(object, name, widget, setChecked, toggled)
+ALBERT_PROPERTY_CONNECT(object, name, widget, setChecked, toggled)
+
+
+///
+/// @brief Convenience macro to connect spinboxes to int user properties
+///
+/// Specialization of ALBERT_PLUGIN_PROPERTY_CONNECT
+///
+/// @param object The object containing the property
+/// @param name The property name
+/// @param widget The widget pointer to connect to
+///
+#define ALBERT_PROPERTY_CONNECT_SPINBOX(object, name, widget) \
+ALBERT_PROPERTY_CONNECT(object, name, widget, setValue, valueChanged)
