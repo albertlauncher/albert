e62f2944ede4458b18318dc58ee05bb3114430da
Manuel Schneider <manuelschneid3r@gmail.com>
2024-09-14 10:49:42 +0200

    Refactor InputHistory
    - More documentation
    - Use private implementation
    
    API:
    - Constructor optionally takes a path

--- include/albert/inputhistory.h
+++ include/albert/inputhistory.h
@@ -3,40 +3,62 @@
 
 #pragma once
 #include <QObject>
-#include <QStringList>
+#include <QString>
 #include <albert/export.h>
+#include <memory>
 
 namespace albert
 {
 
+///
 /// Input history class.
-/// Stores input stings and provides a search iterator.
-class ALBERT_EXPORT InputHistory final : public QObject  /// Input line history for frontends.
+///
+/// Stores input strings and provides a search iterator.
+///
+class ALBERT_EXPORT InputHistory final : public QObject
 {
     Q_OBJECT
+
 public:
-    explicit InputHistory();
+
+    InputHistory(const QString &path = {});
     ~InputHistory() override;
 
+    ///
     /// Add text to history search.
-    /// @note Skips empty strings and drops duplicates.
+    ///
+    /// Skips empty strings and drops duplicates.
+    ///
+    /// @param str The string to add.
+    ///
     Q_INVOKABLE void add(const QString& str);
 
-    /// Next distinct history item.
-    /// @note Skips perfect matches.
-    Q_INVOKABLE QString next(const QString &substring = QString{});
+    ///
+    /// Get next history item matching the pattern.
+    ///
+    /// @param pattern A pattern used to filter the history items.
+    /// @returns The next history item matching the pattern or empty string.
+    ///
+    Q_INVOKABLE QString next(const QString &pattern = QString{});
 
-    /// Previous distinct history item.
-    /// @note Skips perfect matches.
-    Q_INVOKABLE QString prev(const QString &substring = QString{});
+    ///
+    /// Get previous history item matching the pattern.
+    ///
+    /// @param pattern A pattern used to filter the history items.
+    /// @returns The previous history item matching the pattern or empty string.
+    ///
+    Q_INVOKABLE QString prev(const QString &pattern = QString{});
 
+    ///
     /// Reset history search.
+    ///
     Q_INVOKABLE void resetIterator();
 
 private:
-    QString file_path;
-    QStringList lines_;
-    int currentLine_;
+
+    class Private;
+    std::unique_ptr<Private> d;
+
 };
 
 }
--- src/frontend/inputhistory.cpp
+++ src/frontend/inputhistory.cpp
@@ -5,61 +5,81 @@
 #include "util.h"
 #include <QDir>
 #include <QFile>
+#include <QStringList>
 #include <QTextStream>
 using namespace albert;
+using namespace std;
 
-InputHistory::InputHistory() : file_path(QDir(albert::dataLocation()).filePath("albert.history"))
+class InputHistory::Private
 {
-    QFile f(file_path);
-    if (f.open(QIODevice::ReadOnly)){
+public:
+    QString file_path;
+    QStringList lines_;
+    int currentLine_;
+};
+
+
+InputHistory::InputHistory(const QString &path):
+    d(make_unique<Private>())
+{
+    if (path.isEmpty())
+        d->file_path = QDir(albert::dataLocation()).filePath("albert.history");
+    else
+        d->file_path = path;
+
+    if (QFile f(d->file_path); f.open(QIODevice::ReadOnly))
+    {
         QTextStream ts(&f);
         while (!ts.atEnd())
-            lines_ << ts.readLine();
+            d->lines_ << ts.readLine();
         f.close();
-    } else
-        WARN << "Opening history file failed:" << file_path;
+    }
+
     resetIterator();
 }
 
 InputHistory::~InputHistory()
 {
-    QFile f(file_path);
-    if (f.open(QIODevice::WriteOnly)){
+    if (QFile f(d->file_path); f.open(QIODevice::WriteOnly))
+    {
         QTextStream ts(&f);
-        for (const auto &line : lines_)
+        for (const auto &line : d->lines_)
             ts << line << Qt::endl;
         f.close();
     }
+    else
+        WARN << "Writing history file failed:" << d->file_path;
 }
 
 void InputHistory::add(const QString& s)
 {
-    if (!s.isEmpty()){
-        if (lines_.contains(s))
-            lines_.removeAll(s); // Remove dups
-        lines_ << s;
+    if (!s.isEmpty())
+    {
+        if (d->lines_.contains(s))
+            d->lines_.removeAll(s); // Remove dups
+        d->lines_ << s;
     }
     resetIterator();
 }
 
 QString InputHistory::next(const QString &substring)
 {
-    for (int l = currentLine_ - 1; 0 <= l; --l)
+    for (int l = d->currentLine_ - 1; 0 <= l; --l)
         // Simple hack to avoid the seemingly-noop-on-first-history-iteration on disabled clear-on-hide
-        if (lines_[l].contains(substring, Qt::CaseInsensitive) && substring != lines_[l])
-            return lines_[currentLine_ = l];
+        if (d->lines_[l].contains(substring, Qt::CaseInsensitive) && substring != d->lines_[l])
+            return d->lines_[d->currentLine_ = l];
     return QString{};
 }
 
 QString InputHistory::prev(const QString &substring)
 {
-    for (int l = currentLine_ + 1; l < (int)lines_.size(); ++l)
-        if (lines_[l].contains(substring, Qt::CaseInsensitive))
-            return lines_[currentLine_ = l];
+    for (int l = d->currentLine_ + 1; l < (int)d->lines_.size(); ++l)
+        if (d->lines_[l].contains(substring, Qt::CaseInsensitive))
+            return d->lines_[d->currentLine_ = l];
     return QString{};
 }
 
 void InputHistory::resetIterator()
 {
-    currentLine_ = (int)lines_.size();
+    d->currentLine_ = (int)d->lines_.size();
 }
