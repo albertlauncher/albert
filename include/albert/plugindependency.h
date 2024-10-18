5e82a26907ddf26951c0d3e6de0fe750ff10ae6d
Manuel Schneider <manuelschneid3r@gmail.com>
2024-10-18 17:08:25 +0200

    API: Update plugin dependency holders to use the public extension registry.

--- include/albert/plugindependency.h
+++ include/albert/plugindependency.h
@@ -3,6 +3,7 @@
 
 #pragma once
 #include <QCoreApplication>
+#include <albert/albert.h>
 #include <albert/export.h>
 #include <albert/extensionregistry.h>
 #include <albert/logging.h>
@@ -15,64 +16,71 @@ namespace albert
 /// Client code should not use this class directly.
 ///
 template<class T>
-class ALBERT_EXPORT Dependency
+class Dependency
 {
 public:
 
-    inline operator T() const { return dependency; }
-    inline operator bool() const { return dependency != nullptr; }
-    const T* operator->() const { return dependency; }
-    T* operator->() { return dependency; }
-    const T* get() const { return dependency; }
-    T* get() { return dependency; }
+    inline operator T() const { return dependency_; }
+    inline operator bool() const { return dependency_ != nullptr; }
+    const T* operator->() const { return dependency_; }
+    T* operator->() { return dependency_; }
+    const T* get() const { return dependency_; }
+    T* get() { return dependency_; }
 
 protected:
 
-    T *dependency = nullptr;
-
-    explicit Dependency(ExtensionRegistry &registry, const QString &id)
-    {
-        try {
-            auto *e = registry.extensions().at(id);
-            dependency = dynamic_cast<T*>(e);
-            if (!dependency)
-                WARN << QString("Found '%1' but failed casting to expected type.").arg(id);
-        } catch (const std::out_of_range &) {}
-    }
+    T *dependency_ = nullptr;
 };
 
 ///
 /// Convenience holder class for plugin hard dependencies.
 ///
-/// Fetches and holds the weak pointer to the dependecy.
-/// If the dependency is not available, an exception is thrown.
-/// This class is intended to be initialized in plugin constructors.
-/// @note Hard dependencies have to be listed in the plugin metadata, such that
-/// the plugin loader is able to manage the loading order.
+/// Fetches and holds a weak pointer an extension `id` of type `T`. This class is intended to be
+/// initialized on plugin constuction without a try-catch block.
+///
+/// @note Hard dependencies have to be listed in the plugin metadata, such that the plugin loader is
+/// able to manage the loading order.
 ///
 template<class T>
 class ALBERT_EXPORT StrongDependency final : public Dependency<T>
 {
 public:
 
-    explicit StrongDependency(ExtensionRegistry &registry, const QString &id):
-        Dependency<T>(registry, id)
+    ///
+    /// Constructs a StrongDependency with `id`.
+    ///
+    /// @throws std::runtime_error if the dependency is not available or not of type `T`.
+    ///
+    StrongDependency(QString id)
     {
-        if (!this->dependency)
+        try
         {
-            auto m = QCoreApplication::translate(
-                        "Dependency",
-                        "Required dependency '%1' not available.").arg(id);
-            throw std::runtime_error(m.toStdString());
+            this->dependency_ = dynamic_cast<T*>(extensionRegistry().extensions().at(id));
+
+            if (!this->dependency_)
+                throw std::runtime_error(
+                        QCoreApplication::translate(
+                            "Dependency",
+                            "Found '%1' but failed casting to expected type."
+                        ).arg(id).toStdString());
+        }
+        catch (const std::out_of_range &)
+        {
+            throw std::runtime_error(
+                        QCoreApplication::translate(
+                            "Dependency",
+                            "Required dependency '%1' not available."
+                        ).arg(id).toStdString());
         }
     }
 };
 
+
 ///
 /// Convenience holder class for plugin soft dependencies.
 ///
-/// Fetches and holds the weak pointer to the dependecy.
-/// On (de)registration of the dependency the pointer is updated and the callback called.
+/// Watches for (de)registration of an extension `id` of type `T`. On (de)registration of the
+/// dependency the pointer is updated and the callback called.
 ///
 /// If you use this class you may want to lock a mutex against a query handler or an action.
 /// This should not be necessary since the plugin loader and the extension registry must not be
@@ -85,56 +93,23 @@ class ALBERT_EXPORT WeakDependency final : public Dependency<T>
 public:
 
     ///
-    /// @brief WeakDependency constructor
-    /// @param registry The extension registry
-    /// @param id The id of the dependency
-    /// @param registeredCallback A callback that is called when the dependency is (un)registered
+    /// Constructs a WeakDependency with `id` and callback `on_registered`.
     ///
-    explicit WeakDependency(ExtensionRegistry &registry, const QString &id,
-                            std::function<void(bool)> registeredCallback = {}):
-        Dependency<T>(registry, id),
-        callback(registeredCallback)
+    explicit WeakDependency(const QString &id, std::function<void(bool)> on_registered = {}):
+        callback(on_registered),
+        id_(id)
     {
-        conn_add_ = QObject::connect(&registry, &ExtensionRegistry::added, [this](Extension *e)
-        {
-            if (e->id() != id_)
-                return;
+        try {
+            this->dependency_ = dynamic_cast<T*>(extensionRegistry().extensions().at(id));
+            if (!this->dependency_)
+                WARN << QString("Found '%1' but failed casting to expected type.").arg(id);
+        } catch (const std::out_of_range &) { /* okay, optional */ }
 
-            if (!this->dependency)
-            {
-                if (auto *d = dynamic_cast<T*>(e); d)
-                {
-                    this->dependency = d;
-                    if (callback)
-                        callback(true);
-                }
-                else
-                    WARN << QString("Failed casting '%1' to expected type.").arg(id_);
-            }
-            else
-                WARN << "WeakDependency already set. Internal logic error?";
+        conn_add_ = QObject::connect(&albert::extensionRegistry(), &ExtensionRegistry::added,
+                                     [this](Extension *e){ onRegistered(e);});
 
-        });
-
-        conn_rem_ = QObject::connect(&registry, &ExtensionRegistry::removed, [this](Extension *e)
-        {
-            if (e->id() != id_)
-                return;
-
-            if (this->dependency)
-            {
-                if (auto *d = dynamic_cast<T*>(e); d)
-                {
-                    if (callback)
-                        callback(false);  // the dependency should still be usable in the callback
-                    this->dependency = nullptr;
-                }
-                else
-                    WARN << QString("Failed casting '%1' to expected type.").arg(id_);
-            }
-            else
-                WARN << "WeakDependency already unset. Internal logic error?";
-        });
+        conn_rem_ = QObject::connect(&albert::extensionRegistry(), &ExtensionRegistry::removed,
+                                     [this](Extension *e){ onDeregistered(e);});
     }
 
     ~WeakDependency()
@@ -143,17 +118,54 @@ public:
         QObject::disconnect(conn_rem_);
     }
 
-    ///
-    /// @brief (De)Registration callback
-    /// @param bool true if the dependency was added, false if it was removed
-    ///
     std::function<void(bool)> callback;
 
 private:
 
+    void onRegistered(Extension *e)
+    {
+        if (e->id() != this->id_)
+            return;
+
+        if (!this->dependency_)
+        {
+            if (auto *d = dynamic_cast<T*>(e); d)
+            {
+                this->dependency_ = d;
+                if (callback)
+                    callback(true);
+            }
+            else
+                WARN << QString("Failed casting '%1' to expected type.").arg(this->id_);
+        }
+        else
+            CRIT << "WeakDependency already set. Internal logic error?";
+    }
+
+    void onDeregistered(Extension *e)
+    {
+
+        if (e->id() != this->id_)
+            return;
+
+        if (this->dependency_)
+        {
+            if (auto *d = dynamic_cast<T*>(e); d)
+            {
+                if (callback)
+                    callback(false);  // the dependency should still be usable in the callback
+                this->dependency_ = nullptr;
+            }
+            else
+                WARN << QString("Failed casting '%1' to expected type.").arg(this->id_);
+        }
+        else
+            CRIT << "WeakDependency already unset. Internal logic error?";
+    }
+
     QMetaObject::Connection conn_add_;
     QMetaObject::Connection conn_rem_;
-    const QString id_;
+    QString id_;
 
 };
 
