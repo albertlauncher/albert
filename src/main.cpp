50be63751464d94127761cc4672c02fa3811167d
Manuel Schneider <manuelschneid3r@gmail.com>
2025-04-02 22:57:02 +0200

    Catch exceptions in main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,18 @@
-// Copyright (C) 2024 Manuel Schneider
+// Copyright (C) 2024-2025 Manuel Schneider
 
+#include <iostream>
+using namespace std;
 namespace albert {
 extern int run(int, char **);
 }
 
 int main(int argc, char **argv)
 {
-    return albert::run(argc, argv);
+    try {
+        return albert::run(argc, argv);
+    } catch (const exception &e) {
+        cout << e.what() << endl;
+    } catch (...) {
+        cout << "Unknown exception in main!" << endl;
+    }
 }
