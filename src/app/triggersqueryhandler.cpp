c804cf01236125522d9a877c222e3ac35fad791a
Manuel Schneider <manuelschneid3r@gmail.com>
2025-05-27 15:19:06 +0200

    Fix concurrent access to non threadsafe QueryEngine::activeTriggerHandlers()

--- src/app/triggersqueryhandler.cpp
+++ src/app/triggersqueryhandler.cpp
@@ -1,27 +1,37 @@
-// Copyright (c) 2023-2024 Manuel Schneider
+// Copyright (c) 2023-2025 Manuel Schneider
 
 #include "app.h"
 #include "matcher.h"
 #include "queryengine.h"
 #include "standarditem.h"
 #include "triggersqueryhandler.h"
+using namespace Qt::StringLiterals;
 using namespace albert::util;
 using namespace albert;
 using namespace std;
 
-const QStringList TriggersQueryHandler::icon_urls{QStringLiteral(":app_icon")};
-
 TriggersQueryHandler::TriggersQueryHandler(const QueryEngine &query_engine):
-    query_engine_(query_engine) {}
+    query_engine_(query_engine),
+    trigger_handlers_(query_engine.activeTriggerHandlers())
+{
+    // Query engine is not thread safe. Keep a copy.
 
-QString TriggersQueryHandler::id() const
-{ return QStringLiteral("triggers"); }
+    QObject::connect(&query_engine, &QueryEngine::handlerAdded, this, [this]{
+        lock_guard l(trigger_handlers_mutex_);
+        trigger_handlers_ = query_engine_.activeTriggerHandlers();
+    });
 
-QString TriggersQueryHandler::name() const
-{ return QStringLiteral("Triggers"); }
+    QObject::connect(&query_engine, &QueryEngine::handlerRemoved, this, [this]{
+        lock_guard l(trigger_handlers_mutex_);
+        trigger_handlers_ = query_engine_.activeTriggerHandlers();
+    });
+}
 
-QString TriggersQueryHandler::description() const
-{ return tr("Trigger completion items."); }
+QString TriggersQueryHandler::id() const { return u"triggers"_s; }
+
+QString TriggersQueryHandler::name() const { return u"Triggers"_s; }
+
+QString TriggersQueryHandler::description() const { return tr("Trigger completion items."); }
 
 shared_ptr<Item> TriggersQueryHandler::makeItem(const QString &trigger, Extension *handler) const
 {
@@ -30,7 +40,7 @@ shared_ptr<Item> TriggersQueryHandler::makeItem(const QString &trigger, Extensio
         QString(trigger).replace(" ", "•"),
         QString("%1 · %2").arg(handler->name(), handler->description()),
         trigger,
-        {QStringLiteral("gen:?&text=🚀")},
+        {u"gen:?&text=🚀"_s},
         {{
             "set",
             tr("Set input text"),
@@ -40,25 +50,12 @@ shared_ptr<Item> TriggersQueryHandler::makeItem(const QString &trigger, Extensio
     );
 }
 
-void TriggersQueryHandler::handleTriggerQuery(Query &q)
-{
-    auto ris = handleGlobalQuery(q);
-    applyUsageScore(&ris);
-    ranges::sort(ris, greater());
-
-    vector<shared_ptr<Item>> is;
-    is.reserve(ris.size());
-    for (auto &ri : ris)
-        is.emplace_back(::move(ri.item));
-
-    q.add(is);
-}
-
 vector<RankItem> TriggersQueryHandler::handleGlobalQuery(const Query &q)
 {
+    shared_lock l(trigger_handlers_mutex_);
     Matcher matcher(q);
     vector<RankItem> r;
-    for (const auto &[t, h] : query_engine_.activeTriggerHandlers())
+    for (const auto &[t, h] : trigger_handlers_)
         if (const auto m = Matcher(q.string()).match(t, h->name(), h->id()); m)
             r.emplace_back(makeItem(t, h), m);
     return r;
