5616f1706b510674d06e25808c476da24d34b211
Manuel Schneider <manuelschneid3r@gmail.com>
2025-06-10 14:11:13 +0200

    Add `UrlHandler` documentation

--- include/albert/urlhandler.h
+++ include/albert/urlhandler.h
@@ -7,11 +7,18 @@ class QUrl;
 
 namespace albert
 {
+
+/// Albert scheme URL handler interface.
+///
+/// Use this interface to register `albert:` URL handlers based on \ref Extension::id.
+/// URLs with the host matching this extension's id are passed to the \ref handle() method.
+/// E.g. the URL `albert://github/?…` will be redirected to the GitHub extension.
 class ALBERT_EXPORT UrlHandler : virtual public Extension
 {
 public:
 
-    virtual void handle(const QUrl&) = 0;
+    /// Handles the _url_ received.
+    virtual void handle(const QUrl &url) = 0;
 
 protected:
 
