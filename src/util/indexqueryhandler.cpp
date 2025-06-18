00f9eca4ab59b4eb570e62a8babafcc256ad84f3
Manuel Schneider <manuelschneid3r@gmail.com>
2025-06-18 14:12:26 +0200

    Make index in indexqueryhandler non nullable
    Avoids a complicated protocol to avoid segfaults

--- src/util/indexqueryhandler.cpp
+++ src/util/indexqueryhandler.cpp
@@ -1,9 +1,8 @@
-// Copyright (c) 2023-2024 Manuel Schneider
+// Copyright (c) 2023-2025 Manuel Schneider
 
 #include "indexqueryhandler.h"
 #include "itemindex.h"
 #include "query.h"
-#include <memory>
 #include <mutex>
 #include <shared_mutex>
 using namespace albert;
@@ -13,8 +12,8 @@ using namespace util;
 class IndexQueryHandler::Private
 {
 public:
-    unique_ptr<ItemIndex> index;
-    std::shared_mutex index_mutex;
+    shared_mutex index_mutex;
+    ItemIndex index;
 };
 
 IndexQueryHandler::IndexQueryHandler() : d(new Private()) {}
@@ -23,36 +22,27 @@ IndexQueryHandler::~IndexQueryHandler() = default;
 
 void IndexQueryHandler::setIndexItems(vector<IndexItem> &&index_items)
 {
-    // Pointer check not necessary since never called before setFuzzyMatching
-    unique_lock l(d->index_mutex);
-    d->index->setItems(::move(index_items));
+    scoped_lock l(d->index_mutex);
+    d->index.setItems(::move(index_items));
 }
 
 vector<RankItem> IndexQueryHandler::handleGlobalQuery(const Query &query)
 {
-    // Pointer check not necessary since never called before setFuzzyMatching
     shared_lock l(d->index_mutex);
-    return d->index->search(query.string(), query.isValid());
+    return d->index.search(query.string(), query.isValid());
 }
 
 bool IndexQueryHandler::supportsFuzzyMatching() const { return true; }
 
 void IndexQueryHandler::setFuzzyMatching(bool fuzzy)
 {
-    if (!d->index)
+    d->index_mutex.lock();
+    if (d->index.config().fuzzy != fuzzy)
     {
-        auto c = MatchConfig{.fuzzy = fuzzy};
-        d->index = make_unique<ItemIndex>(c);
-        updateIndexItems();
-    }
-    else if ((bool)d->index->config().fuzzy != fuzzy)
-    {
-        auto c = d->index->config();
-        c.fuzzy = fuzzy;
-
-        d->index_mutex.lock();
-        d->index = make_unique<ItemIndex>(c);
+        d->index = ItemIndex({.fuzzy = fuzzy});
         d->index_mutex.unlock();
         updateIndexItems();
     }
+    else
+        d->index_mutex.unlock();
 }
