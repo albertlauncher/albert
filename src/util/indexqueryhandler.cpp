e2b1438a2eba41c33e679e733b02fda013efe576
Manuel Schneider <manuelschneid3r@gmail.com>
2025-06-20 01:35:21 +0200

    Fix index initialization
    This reverts commit 00f9eca4ab59b4eb570e62a8babafcc256ad84f3.

--- src/util/indexqueryhandler.cpp
+++ src/util/indexqueryhandler.cpp
@@ -1,19 +1,20 @@
-// Copyright (c) 2023-2025 Manuel Schneider
+// Copyright (c) 2023-2024 Manuel Schneider
 
 #include "indexqueryhandler.h"
 #include "itemindex.h"
 #include "query.h"
+#include <memory>
 #include <mutex>
 #include <shared_mutex>
+using namespace albert::util;
 using namespace albert;
 using namespace std;
-using namespace util;
 
 class IndexQueryHandler::Private
 {
 public:
-    shared_mutex index_mutex;
-    ItemIndex index;
+    unique_ptr<ItemIndex> index;
+    std::shared_mutex index_mutex;
 };
 
 IndexQueryHandler::IndexQueryHandler() : d(new Private()) {}
@@ -23,13 +24,16 @@ IndexQueryHandler::~IndexQueryHandler() = default;
 void IndexQueryHandler::setIndexItems(vector<IndexItem> &&index_items)
 {
     scoped_lock l(d->index_mutex);
-    d->index.setItems(::move(index_items));
+    if (d->index)
+        d->index->setItems(::move(index_items));
 }
 
 vector<RankItem> IndexQueryHandler::handleGlobalQuery(const Query &query)
 {
     shared_lock l(d->index_mutex);
-    return d->index.search(query.string(), query.isValid());
+    if (d->index)
+        return d->index->search(query.string(), query.isValid());
+    return {};
 }
 
 bool IndexQueryHandler::supportsFuzzyMatching() const { return true; }
@@ -37,9 +41,10 @@ bool IndexQueryHandler::supportsFuzzyMatching() const { return true; }
 void IndexQueryHandler::setFuzzyMatching(bool fuzzy)
 {
     d->index_mutex.lock();
-    if (d->index.config().fuzzy != fuzzy)
+    if (!d->index  // lazy index init
+        || d->index->config().fuzzy != fuzzy)
     {
-        d->index = ItemIndex({.fuzzy = fuzzy});
+        d->index = make_unique<ItemIndex>(MatchConfig{.fuzzy = fuzzy});
         d->index_mutex.unlock();
         updateIndexItems();
     }
