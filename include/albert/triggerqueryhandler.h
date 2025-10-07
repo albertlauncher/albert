5adcf7932cc4be740c0b9d126dd20ee474b30f25
Manuel Schneider <manuelschneid3r@gmail.com>
2025-10-07 16:35:45 +0200

    docs(TriggerQueryHandler): Update doxygen documentation

--- include/albert/triggerqueryhandler.h
+++ include/albert/triggerqueryhandler.h
@@ -12,7 +12,7 @@ class RankItem;
 ///
 /// Abstract trigger query handler.
 ///
-/// If the trigger matches this handler is the only query handler chosen to
+/// If the trigger matches, this handler is the only query handler chosen to
 /// process the user query. Inherit this class if you dont want your results to
 /// be reordered or if you want to display your items of a long running query
 /// as soon as they are available.
@@ -20,35 +20,34 @@ class RankItem;
 class ALBERT_EXPORT TriggerQueryHandler : virtual public Extension
 {
 public:
-    /// The synopsis, displayed on empty query.
-    /// Use this to give the user hints about accepted query strings.
-    /// @returns Empty string.
+    /// Returns the input hint for the given _query_.
+    /// The returned string will be displayed in the input line if space permits.
+    /// The base class implementation returns an empty string.
     virtual QString synopsis(const QString &query) const;
 
-    /// Enable user remapping of the trigger.
-    /// @returns False.
+    /// Returns `true` if the user is allowed to set a custom trigger, otherwise returns `false`.
+    /// The base class implementation returns `true`.
     virtual bool allowTriggerRemap() const;
 
-    /// The default (not user defined) trigger.
-    /// @returns Extension::id().
+    /// Returns the default trigger.
+    /// The base class implementation returns \ref Extension::id() with a space appended.
     virtual QString defaultTrigger() const;
 
-    /// Setter for the user defined trigger.
-    /// Reimplement this if you need this information.
-    /// @since 0.24
-    virtual void setTrigger(const QString &);
+    /// Notifies that the user-defined trigger has changed to _trigger_.
+    /// The base class implementation does nothing.
+    virtual void setTrigger(const QString &trigger);
 
-    /// Fuzzy matching capability.
-    /// @returns False.
+    /// Returns `true` if the handler supports error tolerant matching, otherwise returns `false`.
+    /// The base class implementation returns `false`.
     virtual bool supportsFuzzyMatching() const;
 
-    /// Fuzzy matching behavior.
-    /// Default does nothing.
+    /// Sets the fuzzy matching mode to _enabled_.
+    /// The base class implementation does nothing.
     virtual void setFuzzyMatching(bool enabled);
 
-    /// The trigger query processing function.
+    /// Handles the triggered _query_.
     /// @note Executed in a worker thread.
-    virtual void handleTriggerQuery(Query &) = 0;
+    virtual void handleTriggerQuery(Query &query) = 0;
 
     /// Modifies the score of _rank_items_ to reflect the users usage history.
     void applyUsageScore(std::vector<RankItem> &rank_items);
