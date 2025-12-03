b77b9253aff0e26d718a953020b8191efa25f6d4
Manuel Schneider <manuelschneid3r@gmail.com>
2025-12-03 16:21:48 +0100

    chore(StandardIconType): Add values and documentation

--- include/albert/iconutil.h
+++ include/albert/iconutil.h
@@ -85,85 +85,85 @@ ALBERT_EXPORT std::unique_ptr<Icon> makeFileTypeIcon(const std::filesystem::path
 ///
 enum StandardIconType
 {
-    TitleBarMenuButton,
-    TitleBarMinButton,
-    TitleBarMaxButton,
-    TitleBarCloseButton,
-    TitleBarNormalButton,
-    TitleBarShadeButton,
-    TitleBarUnshadeButton,
-    TitleBarContextHelpButton,
-    DockWidgetCloseButton,
-    MessageBoxInformation,
-    MessageBoxWarning,
-    MessageBoxCritical,
-    MessageBoxQuestion,
-    DesktopIcon,
-    TrashIcon,
-    ComputerIcon,
-    DriveFDIcon,
-    DriveHDIcon,
-    DriveCDIcon,
-    DriveDVDIcon,
-    DriveNetIcon,
-    DirOpenIcon,
-    DirClosedIcon,
-    DirLinkIcon,
-    DirLinkOpenIcon,
-    FileIcon,
-    FileLinkIcon,
-    ToolBarHorizontalExtensionButton,
-    ToolBarVerticalExtensionButton,
-    FileDialogStart,
-    FileDialogEnd,
-    FileDialogToParent,
-    FileDialogNewFolder,
-    FileDialogDetailedView,
-    FileDialogInfoView,
-    FileDialogContentsView,
-    FileDialogListView,
-    FileDialogBack,
-    DirIcon,
-    DialogOkButton,
-    DialogCancelButton,
-    DialogHelpButton,
-    DialogOpenButton,
-    DialogSaveButton,
-    DialogCloseButton,
-    DialogApplyButton,
-    DialogResetButton,
-    DialogDiscardButton,
-    DialogYesButton,
-    DialogNoButton,
-    ArrowUp,
-    ArrowDown,
-    ArrowLeft,
-    ArrowRight,
-    ArrowBack,
-    ArrowForward,
-    DirHomeIcon,
-    CommandLink,
-    VistaShield,
-    BrowserReload,
-    BrowserStop,
-    MediaPlay,
-    MediaStop,
-    MediaPause,
-    MediaSkipForward,
-    MediaSkipBackward,
-    MediaSeekForward,
-    MediaSeekBackward,
-    MediaVolume,
-    MediaVolumeMuted,
-    LineEditClearButton,
-    DialogYesToAllButton,
-    DialogNoToAllButton,
-    DialogSaveAllButton,
-    DialogAbortButton,
-    DialogRetryButton,
-    DialogIgnoreButton,
-    RestoreDefaultsButton,
-    TabCloseButton
+    TitleBarMinButton = 1,                 ///< Menu button on a title bar.
+    TitleBarMenuButton = 0,                ///< Minimize button on title bars (e.g., in QMdiSubWindow).
+    TitleBarMaxButton = 2,                 ///< Maximize button on title bars.
+    TitleBarCloseButton = 3,               ///< Close button on title bars.
+    TitleBarNormalButton = 4,              ///< Normal (restore) button on title bars.
+    TitleBarShadeButton = 5,               ///< Shade button on title bars.
+    TitleBarUnshadeButton = 6,             ///< Unshade button on title bars.
+    TitleBarContextHelpButton = 7,         ///< The Context help button on title bars.
+    MessageBoxInformation = 9,             ///< The "information" icon.
+    MessageBoxWarning = 10,                ///< The "warning" icon.
+    MessageBoxCritical = 11,               ///< The "critical" icon.
+    MessageBoxQuestion = 12,               ///< The "question" icon.
+    DesktopIcon = 13,                      ///< The "desktop" icon.
+    TrashIcon = 14,                        ///< The "trash" icon.
+    ComputerIcon = 15,                     ///< The "My computer" icon.
+    DriveFDIcon = 16,                      ///< The floppy icon.
+    DriveHDIcon = 17,                      ///< The harddrive icon.
+    DriveCDIcon = 18,                      ///< The CD icon.
+    DriveDVDIcon = 19,                     ///< The DVD icon.
+    DriveNetIcon = 20,                     ///< The network icon.
+    DirHomeIcon = 56,                      ///< The home directory icon.
+    DirOpenIcon = 21,                      ///< The open directory icon.
+    DirClosedIcon = 22,                    ///< The closed directory icon.
+    DirIcon = 38,                          ///< The directory icon.
+    DirLinkIcon = 23,                      ///< The link to directory icon.
+    DirLinkOpenIcon = 24,                  ///< The link to open directory icon.
+    FileIcon = 25,                         ///< The file icon.
+    FileLinkIcon = 26,                     ///< The link to file icon.
+    FileDialogStart = 29,                  ///< The "start" icon in a file dialog.
+    FileDialogEnd = 30,                    ///< The "end" icon in a file dialog.
+    FileDialogToParent = 31,               ///< The "parent directory" icon in a file dialog.
+    FileDialogNewFolder = 32,              ///< The "create new folder" icon in a file dialog.
+    FileDialogDetailedView = 33,           ///< The detailed view icon in a file dialog.
+    FileDialogInfoView = 34,               ///< The file info icon in a file dialog.
+    FileDialogContentsView = 35,           ///< The contents view icon in a file dialog.
+    FileDialogListView = 36,               ///< The list view icon in a file dialog.
+    FileDialogBack = 37,                   ///< The back arrow in a file dialog.
+    DockWidgetCloseButton = 8,             ///< Close button on dock windows (see also QDockWidget).
+    ToolBarHorizontalExtensionButton = 27, ///< Extension button for horizontal toolbars.
+    ToolBarVerticalExtensionButton = 28,   ///< Extension button for vertical toolbars.
+    DialogOkButton = 39,                   ///< Icon for a standard OK button in a QDialogButtonBox.
+    DialogCancelButton = 40,               ///< Icon for a standard Cancel button in a QDialogButtonBox.
+    DialogHelpButton = 41,                 ///< Icon for a standard Help button in a QDialogButtonBox.
+    DialogOpenButton = 42,                 ///< Icon for a standard Open button in a QDialogButtonBox.
+    DialogSaveButton = 43,                 ///< Icon for a standard Save button in a QDialogButtonBox.
+    DialogCloseButton = 44,                ///< Icon for a standard Close button in a QDialogButtonBox.
+    DialogApplyButton = 45,                ///< Icon for a standard Apply button in a QDialogButtonBox.
+    DialogResetButton = 46,                ///< Icon for a standard Reset button in a QDialogButtonBox.
+    DialogDiscardButton = 47,              ///< Icon for a standard Discard button in a QDialogButtonBox.
+    DialogYesButton = 48,                  ///< Icon for a standard Yes button in a QDialogButtonBox.
+    DialogNoButton = 49,                   ///< Icon for a standard No button in a QDialogButtonBox.
+    ArrowUp = 50,                          ///< Icon arrow pointing up.
+    ArrowDown = 51,                        ///< Icon arrow pointing down.
+    ArrowLeft = 52,                        ///< Icon arrow pointing left.
+    ArrowRight = 53,                       ///< Icon arrow pointing right.
+    ArrowBack = 54,                        ///< Equivalent to SP_ArrowLeft when the current layout direction is Qt::LeftToRight, otherwise SP_ArrowRight.
+    ArrowForward = 55,                     ///< Equivalent to SP_ArrowRight when the current layout direction is Qt::LeftToRight, otherwise SP_ArrowLeft.
+    CommandLink = 57,                      ///< Icon used to indicate a Vista style command link glyph.
+    VistaShield = 58,                      ///< Icon used to indicate UAC prompts on Windows Vista. This will return a null pixmap or icon on all other platforms.
+    BrowserReload = 59,                    ///< Icon indicating that the current page should be reloaded.
+    BrowserStop = 60,                      ///< Icon indicating that the page loading should stop.
+    MediaPlay = 61,                        ///< Icon indicating that media should begin playback.
+    MediaStop = 62,                        ///< Icon indicating that media should stop playback.
+    MediaPause = 63,                       ///< Icon indicating that media should pause playback.
+    MediaSkipForward = 64,                 ///< Icon indicating that media should skip forward.
+    MediaSkipBackward = 65,                ///< Icon indicating that media should skip backward.
+    MediaSeekForward = 66,                 ///< Icon indicating that media should seek forward.
+    MediaSeekBackward = 67,                ///< Icon indicating that media should seek backward.
+    MediaVolume = 68,                      ///< Icon indicating a volume control.
+    MediaVolumeMuted = 69,                 ///< Icon indicating a muted volume control.
+    LineEditClearButton = 70,              ///< Icon for a standard clear button in a QLineEdit.
+    DialogYesToAllButton = 71,             ///< Icon for a standard YesToAll button in a QDialogButtonBox.
+    DialogNoToAllButton = 72,              ///< Icon for a standard NoToAll button in a QDialogButtonBox.
+    DialogSaveAllButton = 73,              ///< Icon for a standard SaveAll button in a QDialogButtonBox.
+    DialogAbortButton = 74,                ///< Icon for a standard Abort button in a QDialogButtonBox.
+    DialogRetryButton = 75,                ///< Icon for a standard Retry button in a QDialogButtonBox.
+    DialogIgnoreButton = 76,               ///< Icon for a standard Ignore button in a QDialogButtonBox.
+    RestoreDefaultsButton = 77,            ///< Icon for a standard RestoreDefaults button in a QDialogButtonBox.
+    TabCloseButton = 78,                   ///< Icon for the close button in the tab of a QTabBar.
 };
 
 
