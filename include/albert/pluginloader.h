76b66c5f98d7bcc6da5105a51d60c8254802a258
Manuel Schneider <manuelschneid3r@gmail.com>
2026-01-11 00:49:23 +0100

    docs(PluginLoader): Move contract into class description

--- include/albert/pluginloader.h
+++ include/albert/pluginloader.h
@@ -12,7 +12,21 @@ class PluginInstance;
 class PluginMetadata;
 
 ///
-/// Asynchronous plugin loader turning a physical plugin into a logical plugin instance.
+/// Asynchronous plugin loader.
+///
+/// Turns a physical plugin into a logical plugin instance.
+///
+/// Errors are intentionally not logged. Thats the responsibility of the plugin implementation. On
+/// errors, implementations are expected to throw a localized message and print english logs using
+/// their logging category.
+///
+/// Implementations have to set \ref current_loader before calling the constructor of the plugin
+/// instance. This avoids injection mechanisms and therefore reduces boilerplate for \ref
+/// PluginInstance implementations.
+///
+/// Implementations have to emit \ref finished(), when the loading process finished. On success \ref
+/// instance() returns a valid \ref PluginInstance. _info_ contains additional information. On error
+/// \ref instance() returns a nullptr, i.e. the plugin failed to load, _info_ is an error message.
 ///
 /// \ingroup core_plugin
 ///
@@ -21,52 +35,27 @@ class ALBERT_EXPORT PluginLoader : public QObject
     Q_OBJECT
 
 public:
-
-    ///
     /// Returns the path to the plugin.
-    ///
     virtual QString path() const = 0;
 
-    ///
     /// Returns the plugin metadata.
-    ///
     virtual const PluginMetadata &metadata() const = 0;
 
-    ///
-    /// Starts loading the plugin.
-    ///
-    /// @sa \ref current_loader
-    ///
+    /// Starts asynchronous loading process of the plugin.
     virtual void load() = 0;
 
-    ///
     /// Unloads the plugin.
-    ///
     virtual void unload() = 0;
 
-    ///
     /// Returns the \ref PluginInstance if the plugin is loaded, else `nullptr`.
-    ///
     virtual albert::PluginInstance *instance() = 0;
 
-    ///
-    /// Used to set the plugin loader while plugin instatiation.
-    ///
-    /// This avoids injection mechanisms and therefore reduces boilerplate for \ref PluginInstance
-    /// implementations. Implementations have to set this before calling the constructor of the
-    /// plugin instance.
-    ///
+    /// The static injection pointer.
     static thread_local PluginLoader *current_loader;
 
 signals:
 
-    ///
     /// Emitted when the loading process finished.
-    ///
-    /// On success \ref instance() returns a valid \ref PluginInstance. _info_ contains additional
-    /// information about the finished loading proc \ref instance() returns a nullptr, i.e. the
-    /// plugin failed to load, _info_ is interpreted as error message.
-    ///
     void finished(QString info);
 
 protected:
