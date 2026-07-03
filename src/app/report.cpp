b3311fdeee22d388bd1c407ff54dbd1bf7d3eb85
Manuel Schneider <manuelschneid3r@gmail.com>
2026-07-03 23:10:32 +0200

    refactor: Minor refactorings

--- src/app/report.cpp
+++ src/app/report.cpp
@@ -10,51 +10,50 @@
 #include <QStyle>
 #include <QStyleFactory>
 
+static QString fmt (const QString &label, const QString &value)
+{ return QString("%1: %2").arg(label, 21).arg(value); };
+
 QStringList report()
 {
-    auto fn = [](const QString &label, const QString &value)
-    { return QString("%1: %2").arg(label, 21).arg(value); };
-
     QStringList sl;
 
     // BUILD
-    sl << fn("Albert version",        QApplication::applicationVersion());
-    sl << fn("Build date",            __DATE__ " " __TIME__);
-    sl << fn("Qt version",            qVersion());
-    sl << fn("Build ABI",             QSysInfo::buildAbi());
-    sl << fn("Build architecture",    QSysInfo::buildCpuArchitecture());
+    sl << fmt("Albert version",        QApplication::applicationVersion());
+    sl << fmt("Build date",            __DATE__ " " __TIME__);
+    sl << fmt("Qt version",            qVersion());
+    sl << fmt("Build ABI",             QSysInfo::buildAbi());
+    sl << fmt("Build architecture",    QSysInfo::buildCpuArchitecture());
 
     // SYSTEM
-    sl << fn("CPU architecture",      QSysInfo::currentCpuArchitecture());
-    sl << fn("Kernel type",           QSysInfo::kernelType());
-    sl << fn("Kernel version",        QSysInfo::kernelVersion());
-    sl << fn("OS",                    QSysInfo::prettyProductName());
-    sl << fn("OS type",               QSysInfo::productType());
-    sl << fn("OS version",            QSysInfo::productVersion());
+    sl << fmt("CPU architecture",      QSysInfo::currentCpuArchitecture());
+    sl << fmt("Kernel type",           QSysInfo::kernelType());
+    sl << fmt("Kernel version",        QSysInfo::kernelVersion());
+    sl << fmt("OS",                    QSysInfo::prettyProductName());
+    sl << fmt("OS type",               QSysInfo::productType());
+    sl << fmt("OS version",            QSysInfo::productVersion());
 
     // PLATFORM, QT CONFIG
-    sl << fn("Platform name",         QGuiApplication::platformName());
-    sl << fn("Style name",            QApplication::style()->objectName());
-    sl << fn("Available styles",      QStyleFactory::keys().join(", "));
-    sl << fn("Icon theme",            QIcon::themeName());
-    sl << fn("Font",                  QGuiApplication::font().toString());
+    sl << fmt("Platform name",         QGuiApplication::platformName());
+    sl << fmt("Style name",            QApplication::style()->objectName());
+    sl << fmt("Available styles",      QStyleFactory::keys().join(", "));
+    sl << fmt("Icon theme",            QIcon::themeName());
+    sl << fmt("Font",                  QGuiApplication::font().toString());
     QMetaEnum metaEnum = QMetaEnum::fromType<QLocale::Language>();
     QLocale loc;
-    sl << fn("Language",              metaEnum.valueToKey(loc.language()));
-    sl << fn("Locale",                loc.name());
-    sl << fn("Ui languages",          loc.uiLanguages().join(", "));
+    sl << fmt("Language",              metaEnum.valueToKey(loc.language()));
+    sl << fmt("Locale",                loc.name());
+    sl << fmt("Ui languages",          loc.uiLanguages().join(", "));
 
     // APP
-    sl << fn("Binary location",       QApplication::applicationFilePath());
-    sl << fn("Working dir",           QDir::currentPath());
-    sl << fn("Arguments",             QApplication::arguments().join(" "));
+    sl << fmt("Binary location",       QApplication::applicationFilePath());
+    sl << fmt("Working dir",           QDir::currentPath());
+    sl << fmt("Arguments",             QApplication::arguments().join(" "));
 
     // ENVIRONMENT
     sl << "ENVIRONMENT:";
-    auto env = QProcessEnvironment::systemEnvironment();
-    for (const auto &key : env.keys())
-        sl << fn(key, env.value(key));
+    for (auto env = QProcessEnvironment::systemEnvironment();
+         const auto &key : env.keys())
+        sl << fmt(key, env.value(key));
 
     return sl;
 }
-
